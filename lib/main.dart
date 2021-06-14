ef44b6ac6cab0be8389b1fc04e3dc0889fe5cd9e
MorDvash <mordvash1@gmail.com>
2021-06-14 08:08:15 +0300

    Great Place Provider&models

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/provider/greatPlacesProvider.dart';
+import 'package:great_places/screen/detailsScreen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider.value(
+      value: GreatPlacesProvider(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
       ),
-      home: ...,
     );
   }
 }
