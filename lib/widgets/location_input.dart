c77dac536e58beae6a41d45e4e815e55fbf0ad9d
MorDvash <mordvash1@gmail.com>
2021-06-19 17:58:44 +0300

    Allow to pick location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:great_places/middleware/location_helper.dart';
 import 'package:great_places/screen/map_screen.dart';
 import 'package:location/location.dart';
@@ -25,7 +26,7 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(
+    final selectedLocation = await Navigator.of(context).push<LatLng>(
       MaterialPageRoute(
         fullscreenDialog: true,
         builder: (ctx) => MapScreen(
@@ -36,7 +37,7 @@ class _LocationInputState extends State<LocationInput> {
     if (selectedLocation == null) {
       return;
     }
-    // ...
+    print(selectedLocation);
   }
 
   @override
